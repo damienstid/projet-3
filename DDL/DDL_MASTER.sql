27c8d9048b656abb9915f079307f8744b7e61bee
SimonSTID <43846246+SimonSTID@users.noreply.github.com>
2019-02-26 13:44:01 +0100

    Update DDL_MASTER.sql

--- DDL/DDL_MASTER.sql
+++ DDL/DDL_MASTER.sql
@@ -1,9 +1,13 @@
 CREATE TABLE MASTER(
-    code INT PRIMARY KEY,
-    secteur_activité  INT,
-    departement  INT,
-    ville VARCHAR,
-    nom_etablissement VARCHAR
+code INT PRIMARY KEY,
+type_diplome TEXT,
+q6_7 TEXT, -- type d’employeur
+q6_8 TEXT, -- secteur activite
+q6_9a TEXT, -- departement
+q6_9b TEXT, -- pays
+q6_9c TEXT, -- ville
+q6_14_6 TEXT -- nom etablissement
 );
+
 
 
