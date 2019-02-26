15d308a4f90a9dd8cd7cc672398c5b05d0e95614
SimonSTID <43846246+SimonSTID@users.noreply.github.com>
2019-02-26 16:30:25 +0100

    Update Algorithme_Levenshtein.py

--- Algorithme de Levenshtein/Algorithme_Levenshtein.py
+++ Algorithme de Levenshtein/Algorithme_Levenshtein.py
@@ -1,22 +1,13 @@
-'''pour chaque ligne du fichier LP faire
-    chercher la (ou les) communes dont le nom est le plus proche
-    pour chacunes de ces communes faire
-        chercher dans communes l'établissement de cette commune dont le nom est le plus proche
-fin pour
-fin pour'''
-
-##################################################################
-
 ### Fonction Levenshtein ###
 def levenshtein(mot1,mot2):
-	ligne_i = [ k for k in range(len(mot1)+1)]
-	for i in range(1, len(mot2) + 1):
-		ligne_prec = ligne_i
-		ligne_i = [i]*(len(mot1)+1)
-		for k in range(1,len(ligne_i)):
-			cout = int(mot1[k-1] != mot2[i-1])
-			ligne_i[k] = min(ligne_i[k-1] + 1, ligne_prec[k] + 1, ligne_prec[k-1] + cout)
-	return ligne_i[len(mot1)]
+    ligne_i = [ k for k in range(len(mot1)+1)]
+    for i in range(1, len(mot2) + 1):
+        ligne_prec = ligne_i
+        ligne_i = [i]*(len(mot1)+1)
+        for k in range(1,len(ligne_i)):
+            cout = int(mot1[k-1] != mot2[i-1])
+            ligne_i[k] = min(ligne_i[k-1] + 1, ligne_prec[k] + 1, ligne_prec[k-1] + cout)
+    return ligne_i[len(mot1)]
 
 ### Code python ###
 
@@ -25,24 +16,43 @@ import sqlite3
 liste = []
 
 conn = sqlite3.connect('data.db')
-c = conn.cursor()
+c_LP = conn.cursor()
 
-for row_LP in c.execute('SELECT ville FROM LP'):
-	print(row_LP)
-	distanceMinimum=100
-	for row_communes in c.execute('SELECT ville FROM communes'):
-		print(row_LP)
-		print(row_communes)
-		print(levenshtein(row_LP,row_communes))
-		if levenshtein(row_LP,row_communes) < distanceMinimum:
-			distanceMinimum = levenshtein(row_LP,row_communes)
-			correspondance = row_communes
-	liste.append(correspondance)
-        
-print(liste)
+#requeteSupprCol  = "ALTER TABLE LP DROP COLUMN villeSIRENE"
+#c.execute(requeteSupprCol)
+#requeteAjoutColonne = "ALTER TABLE LP ADD villeSIRENE VARCHAR(100)"
+#c.execute(requeteAjoutColonne)
 
+
+for row_LP in c_LP.execute('SELECT * FROM LP WHERE q6_9a IS NOT NULL AND q6_9c IS NOT NULL'):
+    c_communes = conn.cursor()
+    distanceMinimum=1000
+    requete = 'SELECT * FROM communes where departement = \'' + row_LP[4] + '\''
+    for row_communes in c_communes.execute(requete):
+        ville_lp = row_LP[6].lower()
+        ville_lp = ville_lp.strip()
+        ville_lp = ville_lp.replace("-", " ")
+        ville_com = row_communes[2].lower()
+        ville_com = ville_com.replace("-", " ")
+        if ville_lp in ['cherbourg', 'octeville']:
+            ville_lp = 'cherbourg octeville'
+        elif ville_lp in ['beaumont', 'la hague']:
+            ville_lp = 'beaumont hague'
+        if levenshtein(ville_lp, ville_com) < distanceMinimum:
+            distanceMinimum = levenshtein(ville_lp, ville_com)
+            code_insee = row_communes[0]
+            nom_commune = row_communes[2].replace('\'', '\'\'')
+            latitude = str(row_communes[4])
+            longitude = str(row_communes[3])
+    if distanceMinimum < 3:
+        requete = 'UPDATE LP SET CODE_INSEE = \'' + code_insee + '\', '
+        requete += 'LATITUDE = ' + latitude + ', LONGITUDE = ' + longitude + ', '
+        requete += 'NOM_COM = \'' + nom_commune + '\' WHERE CODE = \'' + row_LP[0] + '\''
+        # print(row_LP[0], row_LP[6], nom_commune, distanceMinimum, sep=",")
+        # print(requete)
+        c_communes.execute(requete)
+      
 conn.commit()
 conn.close()
-
 
 
