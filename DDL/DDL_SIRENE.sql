90d4d95957fd7aa7b8d0e0bd4aff30f4be5695b0
SimonSTID <43846246+SimonSTID@users.noreply.github.com>
2019-02-27 08:52:33 +0100

    Update DDL_SIRENE.sql

--- DDL/DDL_SIRENE.sql
+++ DDL/DDL_SIRENE.sql
@@ -1,15 +1,3 @@
-CREATE TABLE SIRENE (
-code_entreprise VARCHAR,
-code_etablissement VARCHAR,
-departement VARCHAR,
-ville VARCHAR(100),
-nom_etablissement VARCHAR(100),
-code_naf VARCHAR
-);
-
-CREATE INDEX departement_index ON SIRENE(departement);
-
-
 
 
 CREATE TABLE SIRENE (
